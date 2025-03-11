8aa1cff1e96936fea508fabe87270923738ad85c
Younglad-Paul <borisadedayopaul@gmail.com>
2025-03-11 19:09:59 +0100

    fetched the about page from live

--- src/Dashboard/dashboard/CMS/About/AboutCMS.tsx
+++ src/Dashboard/dashboard/CMS/About/AboutCMS.tsx
@@ -1,46 +1,31 @@
 import { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Trash2, Edit, Plus, Search } from 'lucide-react';
+import { Trash2, Edit, Plus, Search, Loader2 } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { useGetAboutQuery } from '@/app/api';
 
 
 interface AboutSection {
     id: string;
-    title: string;
-    subtitle?: string;
-    content: string;
-    image?: string;
+    vision: string;
+    visionPhoto:{
+    url: string;
+    }
+    mission?: string;
+    missionPhoto:{
+        url: string;
+        }
 }
-
-
-export const dummyAboutContent =
-    {
-        id: '1',
-        title: 'About us',
-        subtitle: 'Dedicated to excellence in education',
-        content: "At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development",
-        image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
-    }
-
-export const dummyMissionContent = 
-    {
-        id: '1',
-        title: 'Our Mission',
-        subtitle: 'Dedicated to excellence in education',
-        content: "At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development. At our institution, we are committed to fostering intellectual growth and personal development",
-        image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
-    }
 
 export default function AboutCMS() {
     const { data, isLoading } = useGetAboutQuery<AboutSection | any | undefined>(undefined);
     const [aboutSections, setAboutSections] = useState<AboutSection | null>(null);
-    const [missionSections, setMissionSections] = useState<AboutSection | null>(null);
+
+    // console.log("data: ", data)
 
     useEffect(() => {
-        setAboutSections(data || dummyAboutContent);
-        setMissionSections(data || dummyMissionContent);
+        setAboutSections(data?.data);
     }, [data]);
 
 
@@ -50,6 +35,12 @@ export default function AboutCMS() {
                 <div className='flex items-center justify-between'>
                     <h1 className="text-3xl font-bold mb-8">About Page Manager</h1>
                 </div>
+
+                {isLoading ? (
+                    <section className='w-full h-screen flex items-center justify-center'>
+                        <Loader2 size={50} className='text-purple-500 animate-spin' />
+                    </section>
+                ):(
 
                 <section className='space-y-6'>
 
@@ -61,9 +52,9 @@ export default function AboutCMS() {
                                     <div className="flex gap-4 w-full">
                                         <div className="flex flex-col justify-between w-full p-2">
                                             <div>
-                                                <h3 className="font-semibold text-xl mb-2">{aboutSections?.title}</h3>
+                                                <h3 className="font-semibold text-xl mb-2">Who we are</h3>
                                                 <p className="text-sm text-gray-600 text-justify">
-                                                    {aboutSections?.content}
+                                                    {aboutSections?.vision}
                                                 </p>
                                             </div>
 
@@ -78,11 +69,11 @@ export default function AboutCMS() {
                                             </div>
                                         </div>
 
-                                        {aboutSections?.image && (
+                                        {aboutSections?.visionPhoto && (
                                             <div className='w-5/12'>
                                                 <img
-                                                    src={aboutSections?.image}
-                                                    alt={aboutSections?.title}
+                                                    src={aboutSections?.visionPhoto?.url}
+                                                    alt={'Who we are'}
                                                     className="w-full h-60 object-cover rounded"
                                                 />
                                             </div>
@@ -100,15 +91,15 @@ export default function AboutCMS() {
                                     <div className="flex gap-4 w-full">
                                         <div className="flex flex-col justify-between w-full p-2">
                                             <div>
-                                                <h3 className="font-semibold text-xl mb-2">{missionSections?.title}</h3>
+                                                <h3 className="font-semibold text-xl mb-2">Our Mission</h3>
                                                 <p className="text-sm text-gray-600 text-justify">
-                                                    {missionSections?.content}
+                                                    {aboutSections?.mission}
                                                 </p>
                                             </div>
 
                                             <div className="flex gap-2 mt-4">
                                                 <Link
-                                                    to={`/dashboard/cms/about/${missionSections?.id}`}
+                                                    to={`/dashboard/cms/about/${aboutSections?.id}`}
                                                     className='flex items-center gap-2 bg-purple-500 hover:bg-purple-600 rounded-md p-2 px-4 text-white'
                                                 >
                                                     <Edit className="w-4 h-4 mr-2" />
@@ -117,11 +108,11 @@ export default function AboutCMS() {
                                             </div>
                                         </div>
 
-                                        {missionSections?.image && (
+                                        {aboutSections?.missionPhoto && (
                                             <div className='w-5/12'>
                                                 <img
-                                                    src={missionSections?.image}
-                                                    alt={missionSections?.title}
+                                                    src={aboutSections?.missionPhoto?.url}
+                                                    alt={"Our mission"}
                                                     className="w-full h-60 object-cover rounded"
                                                 />
                                             </div>
@@ -132,6 +123,9 @@ export default function AboutCMS() {
                     </Card>
                 </div>
                 </section>
+
+                )}
+
             </div>
         </div>
     );
