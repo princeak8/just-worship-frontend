e8f5b10b4b0dce5ba23ea5889c6f156d18243580
Younglad-Paul <borisadedayopaul@gmail.com>
2025-03-12 17:02:29 +0100

    added the live data for event page

--- src/Events/Events.tsx
+++ src/Events/Events.tsx
@@ -1,57 +1,43 @@
 import type React from 'react';
-import {motion} from "motion/react"
+import { motion } from "motion/react"
 import BG from '@/public/gallery/gallery4.jpeg'
+import { useGetEventQuery } from '@/app/api';
+import { Users } from 'lucide-react';
 
-interface Event {
-  id: number;
-  title: string;
-  date: string;
-  description: string;
-  imageUrl: string;
+interface GetEvent {
+  data: Event[]
 }
 
-const events: Event[] = [
-  {
-    id: 1,
-    title: 'Tech Conference 2023',
-    date: '2023-11-15',
-    description: 'Join us for the biggest tech conference of the year!',
-    imageUrl: 'https://imgs.search.brave.com/UZr3dQpJ2-uxH6UrX7zyV4KKzUq48gHuTUZIggptygA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXNp/Z25zaGFjay5uZXQv/d3AtY29udGVudC91/cGxvYWRzL3BsYWNl/a2l0dGVuLmpwZw',
-  },
-  {
-    id: 2,
-    title: 'Art Exhibition',
-    date: '2023-12-01',
-    description: 'Explore the latest in contemporary art.',
-    imageUrl: 'https://imgs.search.brave.com/UZr3dQpJ2-uxH6UrX7zyV4KKzUq48gHuTUZIggptygA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXNp/Z25zaGFjay5uZXQv/d3AtY29udGVudC91/cGxvYWRzL3BsYWNl/a2l0dGVuLmpwZw',
-  },
-  {
-    id: 3,
-    title: 'Music Festival',
-    date: '2024-01-20',
-    description: 'A weekend of live music and performances.',
-    imageUrl: 'https://imgs.search.brave.com/UZr3dQpJ2-uxH6UrX7zyV4KKzUq48gHuTUZIggptygA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXNp/Z25zaGFjay5uZXQv/d3AtY29udGVudC91/cGxvYWRzL3BsYWNl/a2l0dGVuLmpwZw',
-  },
-];
+interface Event {
+  id: string;
+  name: string;
+  date: string;
+  bookings: string;
+  content: string;
+  coverPhoto: {
+    url: string;
+  }
+}
 
 const Events: React.FC = () => {
+  const { data, isLoading } = useGetEventQuery<GetEvent[] | any | undefined>(undefined);
   return (
     <div className="min-h-screen bg-gray-100 lg:px-4 sm:px-6 lg:px-8 py-24">
-       <motion.section 
-        className='h-60 flex items-center mb-12 overflow-hidden' 
-        style={{backgroundImage: `url(${BG})`, backgroundSize: 'cover'}}
+      <motion.section
+        className='h-60 flex items-center mb-12 overflow-hidden'
+        style={{ backgroundImage: `url(${BG})`, backgroundSize: 'cover' }}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
       >
-        <motion.div 
+        <motion.div
           className='slant w-7/12 bg-black bg-opacity-50 h-full flex items-center lg:px-40 text-white'
           initial={{ x: -100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.8, delay: 0.5 }}
         >
           <header className="text-center p-2">
-            <motion.h1 
+            <motion.h1
               className="text-lg lg:text-4xl font-bold mb-3"
               initial={{ y: -20, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
@@ -71,25 +57,27 @@ const Events: React.FC = () => {
       </section> */}
 
       <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-2">
-        {events.map((event, index) => (
-            <motion.div key={index}
+        {data?.data.map((event: any, index: number) => (
+          <motion.div key={index}
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: index * 0.5 }}
-            viewport={{once: true}}
+            viewport={{ once: true }}
 
-            
+
             className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
           >
             <img
-              src={event.imageUrl}
+              src={event.coverPhoto?.url}
               alt={event.title}
               className="w-full h-48 object-cover"
             />
             <div className="p-6">
-              <h2 className="text-xl font-semibold text-gray-900 mb-2">{event.title}</h2>
-              <p className="text-sm text-gray-500 mb-4">{event.date}</p>
-              <p className="text-base text-gray-700">{event.description}</p>
+              <div className='flex items-center justify-between mb-2'>
+                <p className="text-sm text-gray-500">{event.date}</p>
+                <p className="text-sm text-white flex items-center gap-2 bg-purple-500 p-1 px-2 rounded-md"><Users size={15} className='' />{event.bookings.length}</p>
+              </div>
+              <p className="text-base text-gray-700 text-justify">{event.content}</p>
             </div>
           </motion.div>
         ))}
