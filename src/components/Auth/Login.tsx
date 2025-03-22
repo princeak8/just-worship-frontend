08faec8f03063bd57a42bcc2d4dfe620e4b34b84
Younglad-Paul <borisadedayopaul@gmail.com>
2025-03-22 23:34:36 +0100

    temporarily removed the login options and the signup link

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -80,7 +80,7 @@ const Login: React.FC = () => {
                   </Button>
                   ) 
                   }
-                <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                   <span className="relative z-10 bg-background px-2 text-muted-foreground">
                     Or continue with
                   </span>
@@ -113,13 +113,14 @@ const Login: React.FC = () => {
                     </svg>
                     <span className="sr-only">Login with Meta</span>
                   </Button>
-                </div>
-                <div className="text-center text-sm">
+                </div> */}
+                {/* <div className="text-center text-sm">
                   Don&apos;t have an account?{" "}
                   <a href="/signup" className="underline underline-offset-4">
                     Sign up
                   </a>
-                </div>
+                </div> */}
+                
               </div>
             </form>
             
